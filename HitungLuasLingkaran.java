9be4695e67e5bb556647bd107b6daad965dd16ce
minah123z <35888462+minah123z@users.noreply.github.com>
2018-10-19 05:01:01 -0700

    Add files via upload

---
+++ HitungLuasLingkaran.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitung.luas.lingkaran;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class HitungLuasLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_luas_lingkaran Laund = new Frame_luas_lingkaran();
+        Laund.setVisible(true);
+    }
+}
